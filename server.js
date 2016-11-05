6f72c11838aa468714779c7af51d46ef3a11eab9
destromas1 <shahjada.talukdar@gmail.com>
2016-11-06 03:12:52 +0600

    bower loadingfixed

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ var path = require('path');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/bower_components',  express.static(path.join(__dirname , '/bower_components')));
+
 var server = app.listen(process.env.PORT || 5000, function () {
     var host = server.address().address;
     var port = server.address().port;
