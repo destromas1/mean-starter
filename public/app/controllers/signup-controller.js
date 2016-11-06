f0b7747a493d84ddf352090ec26eb53e52949995
destromas1 <shahjada.talukdar@gmail.com>
2016-11-07 05:58:17 +0600

    Forms

--- public/app/controllers/signup-controller.js
+++ public/app/controllers/signup-controller.js
@@ -3,6 +3,13 @@
 app.controller('signupCtrl', ['$scope', function ($scope) {
   console.log('signupCtrl is running...');
 
-  
+  $scope.model = {
+    email: '',
+    password : ''
+  }
+
+  $scope.signUp = function () {
+    console.log('signup...');
+  };
 
 }]);
